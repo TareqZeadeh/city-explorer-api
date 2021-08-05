6837a6b21b3810a70ac00301eb6677043878ee33
Tareq Zeadeh <tareq.ha130@yahoo.com>
2021-08-05 14:07:19 +0300

    lab10 done memory

--- components/Movies.js
+++ components/Movies.js
@@ -1,5 +1,32 @@
 'use strict';
 const Movies = require('../constructors/Moviesconstructor');
+const axios = require('axios');
+require('dotenv').config();
+
+let Memory={};
+function MoviesHandler (req, res) {
+    const cityname = req.query.searchQuery.toLocaleLowerCase();
+    const URL = `${process.env.MOVIE_API_URL}?api_key=${process.env.MOVIE_API_KEY}&query=${cityname}`
+    if(Memory[cityname] !== undefined) 
+  {
+    console.log('get the data from the Memory');
+    res.send(Memory[cityname]);
+  }
+  else{
+    axios
+        .get(URL)
+        .then(result => {
+            console.log('get the data from the API');
+            res.send(moviesforcastobject(result.data));
+
+        })
+        .catch(err => {
+            res.send(err);
+            console.log(err.data)
+        })
+
+
+}
 
 const moviesforcastobject = (moviesobj) => {
     const moviesobjs = [];
@@ -13,7 +40,7 @@ const moviesforcastobject = (moviesobj) => {
         const image_url= process.env.IMG_URL+obj.poster_path;
         const popularity=obj.popularity;
         const released_on=obj.release_date;
-
+        Memory[cityname]=moviesobjs;
         moviesobjs.push(new Movies(title,overview,average_votes,total_votes,image_url,popularity,released_on))
 
     })
@@ -21,7 +48,7 @@ const moviesforcastobject = (moviesobj) => {
     return moviesobjs;
 
 };
+}
 
 
-
-module.exports = moviesforcastobject;
+module.exports = MoviesHandler;
